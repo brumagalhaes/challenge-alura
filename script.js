8a261e5b9af2e3a38ca91eecde1fde6cc7e2a5ac
brumagalhaes <bruna.madur@gmail.com>
2024-08-26 05:05:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+const encrypt = document.getElementById("encrypt");
+const decrypt = document.getElementById("decrypt");
+const copy = document.getElementById("copy");
+
+const textDefault = document.querySelector(".default-text-container");
+const textResult = document.querySelector(".result-text-container");
+const text = document.querySelector(".result-message-text");
+
+
+encrypt.addEventListener("click", () => {
+  let input = document.getElementById("encrypt-input").value;
+  const isValid = /([A-ZáéíóúÁÉÍÓÚñ\d$@$!%*?&])/gm.test(input);
+  if (!isValid && input.length > 0) {
+    const obj = {
+      a: "ai",
+      e: "enter",
+      i: "imes",
+      o: "ober",
+      u: "ufat",
+    };
+    input = input.replace(/e|i|a|o|u/gm, (matched) => {
+      return obj[matched];
+    });
+
+    exitAlert();
+    showResult();
+
+    text.textContent = input;
+  } else {
+    showAlert();
+  }
+});
+
+
+decrypt.addEventListener("click", () => {
+  let input = document.getElementById("encrypt-input").value;
+  const isValid = /([A-ZáéíóúÁÉÍÓÚñ\d$@$!%*?&])/gm.test(input);
+  if (!isValid && input.length > 0) {
+    const obj = {
+      ai: "a",
+      enter: "e",
+      imes: "i",
+      ober: "o",
+      ufat: "u",
+    };
+    input = input.replace(/enter|imes|ai|ober|ufat/gm, (matched) => {
+      return obj[matched];
+    });
+
+    exitAlert();
+    showResult();
+
+    text.textContent = input;
+  } else {
+    showAlert();
+  }
+});
+
+// copiar texto
+copy.addEventListener("click", () => {
+  let copied = text.textContent;
+
+  navigator.clipboard.writeText(copied).then(() => {
+    copy.textContent = "Copiado";
+    copy.classList.add("btn-copied");
+
+    window.setTimeout(() => {
+      copy.textContent = "Copiar";
+      copy.classList.remove("btn-copied");
+    }, 1000);
+  });
+});
+
+const showResult = () => {
+  textDefault.style.display = "none";
+  textResult.style.display = "flex";
+};
+
+const exitAlert = () => {
+  const alert = document.querySelector(".alert-disabled");
+  const alertText = document.querySelector(".encrypt-text");
+  alertText.classList.remove("encrypt-text-alert");
+  alert.classList.remove("alert-actived");
+};
+
+const showAlert = () => {
+  const alertText = document.querySelector(".encrypt-text");
+  const alert = document.querySelector(".alert-disabled");
+  alert.classList.add("alert-actived");
+  alertText.classList.add("encrypt-text-alert");
+};
